965ee7f1836de2c43c9211149de56b6b25182120
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2026-05-13 19:43:12 +0530

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -46,7 +46,7 @@ function executeWidgetCode() {
                              {
                               var topicName = '3DXVertex.stream';
                               var data = { "sender": "3DXVertex", "messsage": "info to share ...",
-                                "streamkey":"v9OJ3Xxm8vbTXfI5mzYDra7sLNn0wlTqaTUW",
+                                "streamkey":"ULuC833t-HkF_tRAJb4YImyoeeh-BaBbBmsB",
                                 "clientid":"08CEF7AE6E675F48D2C802AC0E6AFD183CC95553AA6889F032DD29AB070E40C0",
                                 "objectDisplayName": obj.data.items[0].displayName
                                } ;
